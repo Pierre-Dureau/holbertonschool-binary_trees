f61febebce7704525cdb347cbae4c31cf4835ce5
Pierre Dureau <dureaupierre53@gmail.com>
2022-07-20 13:20:00 +0200

    Edit header

--- binary_trees.h
+++ binary_trees.h
@@ -78,4 +78,7 @@ avl_t *array_to_avl(int *array, size_t size);
 avl_t *avl_remove(avl_t *root, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
 
+/* Heap */
+int binary_tree_is_heap(const binary_tree_t *tree);
+
 #endif /* _BINARY_TREES_H_ */
